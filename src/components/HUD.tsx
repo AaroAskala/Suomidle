30009f32d0b3730b446da16f786b5cc6d15531c6
AaroAskala <107686316+AaroAskala@users.noreply.github.com>
2025-09-09 20:37:31 +0300

    feat: add upgrade data and UI

--- src/components/HUD.tsx
+++ src/components/HUD.tsx
@@ -3,10 +3,11 @@ import { useGameStore } from '../app/store';
 export function HUD() {
   const population = useGameStore((s) => s.population);
   const addPopulation = useGameStore((s) => s.addPopulation);
+  const click = useGameStore((s) => s.clickPower);
   return (
     <div>
       <div>Population: {Math.floor(population)}</div>
-      <button onClick={() => addPopulation(1)}>Click</button>
+      <button onClick={() => addPopulation(click)}>Click</button>
     </div>
   );
 }
