0bc2a1e954fc2f2724cf5406a75faa4a2ca2c88f
AaroAskala <107686316+AaroAskala@users.noreply.github.com>
2025-09-16 16:52:41 +0300

    Add Maailma shop UI with purchase handling

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import { useGameStore } from './app/store';
 import './App.css';
 import { playTierMusic } from './audio/music';
 import { useSettingsStore } from './app/settingsStore';
+import { MaailmaShop } from './ui/MaailmaShop';
 
 function App() {
   const tierLevel = useGameStore((s) => s.tierLevel);
@@ -57,6 +58,7 @@ function App() {
       <BuildingsGrid />
       <TechGrid />
       <Prestige />
+      <MaailmaShop />
     </>
   );
 }
